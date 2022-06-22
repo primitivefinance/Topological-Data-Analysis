21b3cf2d2b70778ab34f94c4bcf271b08e4846f8
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-06-22 14:30:39 -0600

    Coloring works

--- Topological_Data_Analysis_Users/getData.py
+++ Topological_Data_Analysis_Users/getData.py
@@ -6,12 +6,9 @@ from dotenv import load_dotenv
 
 def get_data():
     load_dotenv()
-    ADDRESS_TO_INSPECT = (
-        "0x54522da62a15225c95b01bd61ff58b866c50471f"  # Primitive Manager Address
-    )
     r = requests.get(
         "https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&sort=asc&apikey={}".format(
-            ADDRESS_TO_INSPECT, os.environ["ETHERSCAN_API"]
+            os.environ["PRIMITIVE_MANAGER"], os.environ["ETHERSCAN_API"]
         )
     )
     a = r.json()
@@ -21,7 +18,6 @@ def get_data():
 
 
 def get_users_nieghbors(user):
-    # for user in users:
     r = requests.get(
         "https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&page=1&sort=asc&apikey={}".format(
             user, os.environ["ETHERSCAN_API"]
--- Topological_Data_Analysis_Users/main.py
+++ Topological_Data_Analysis_Users/main.py
@@ -1,13 +1,24 @@
+from platform import node
 from getData import get_data
 from buildGraph import build_graph
 from visualize import visualize
+import networkx as nx
 
 
 def main():
     users = get_data()
-    print(users)
+    print(users.head())
     graph = build_graph(users.head())
-    visualize(graph)
+    color_map = nx.get_node_attributes(graph, "attr")
+    for key in color_map:
+        if color_map[key] == "user":
+            color_map[key] = "green"
+        if color_map[key] == "neighbor":
+            color_map[key] = "blue"
+        if color_map[key] == "primitive":
+            color_map[key] = "purple"
+    node_colors = [color_map.get(node) for node in graph.nodes()]
+    visualize(graph, node_colors)
 
 
 if __name__ == "__main__":
