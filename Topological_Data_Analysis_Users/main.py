de01428b90652cc7925d205c6bbe3db168c01dc3
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-06-22 18:46:55 -0600

    Update main.py

--- Topological_Data_Analysis_Users/main.py
+++ Topological_Data_Analysis_Users/main.py
@@ -9,15 +9,6 @@ def main():
     users = get_data()
     print(users.head())
     graph = build_graph(users.head())
-    # color_map = nx.get_node_attributes(graph, "attr")
-    # for key in color_map:
-    #     if color_map[key] == "user":
-    #         color_map[key] = "green"
-    #     if color_map[key] == "neighbor":
-    #         color_map[key] = "blue"
-    #     if color_map[key] == "primitive":
-    #         color_map[key] = "purple"
-    # node_colors = [color_map.get(node) for node in graph.nodes()]
     visualize(graph)
 
 
